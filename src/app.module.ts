0d81d31ff1a07bdccc8907e9905e1a066e1f587f
kutoo <iblue_me@163.com>
2023-07-17 16:44:01 +0800

    feat:接入配置、数据库连接

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,38 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule, TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
+import * as Joi from 'joi';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ".env",
+      validationSchema: Joi.object({
+        DB_TYPE: Joi.string().valid("mysql", "postgres"),
+        DB_HOST: Joi.string().ip(),
+        DB_PORT: Joi.number().default(3306),
+        DB_USER: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_DATABASE: Joi.string().required(),
+        DB_SYNC: Joi.boolean().default(false),
+      }),
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: configService.get("DB_TYPE"),
+        host: configService.get("DB_HOST"),
+        port: configService.get("DB_PORT"),
+        username: configService.get("DB_USER"),
+        password: configService.get("DB_PASSWORD"),
+        database: configService.get("DB_DATABASE"),
+        synchronize: configService.get("DB_SYNC"),
+        logging:false
+      }) as TypeOrmModuleAsyncOptions,
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
